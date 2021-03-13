f6812329b46b32deecae2ac0f3627ed62861250a
shultztom <shultzthomask@sau.edu>
2021-03-12 22:27:54 -0600

    added web build

--- scripts/dockerBuild.sh
+++ scripts/dockerBuild.sh
@@ -1,3 +1,4 @@
+flutter build web 
 PACKAGE_NAME="ip-addr-flutter"
 docker build -t nexus-docker-internal.shultzlab.com/shultztom/$PACKAGE_NAME:latest .
 docker push nexus-docker-internal.shultzlab.com/shultztom/$PACKAGE_NAME:latest
